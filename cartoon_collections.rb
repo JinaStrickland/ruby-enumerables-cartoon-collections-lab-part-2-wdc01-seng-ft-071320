972356b8dc73b8ef2c0ee201c07702ab14ff4105
Jina Strickland <jina30@gmail.com>
2020-07-03 02:03:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.select do | element |
+  planeteer_calls.find do | element |
     valid_calls.find(element)
   end
 end
